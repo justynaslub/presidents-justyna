069d3117ba2bb2462f58be5085adef47aada2c28
Justyna <justyna.slubowska@interia.pl>
2022-09-10 15:36:25 +0200

    security added

---
+++ src/main/java/com/presidents/configuration/SecurityConfiguration.java
@@ -0,0 +1,47 @@
+package com.presidents.configuration;
+
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+//    @Value("${username}")
+//    private String username;
+//
+//    @Value("${password}")
+//    private String password;
+//
+//    @Value("${role}")
+//    private String role;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/presidents/*").hasAnyRole("ADMIN")
+                .antMatchers(HttpMethod.POST,"/presidents/*").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST,"/save").authenticated()
+                .anyRequest().permitAll()
+                .and().formLogin()
+                .and().httpBasic()
+                .and().logout()
+                .and().csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("{noop}admin").roles("ADMIN");
+    }
+
+}
